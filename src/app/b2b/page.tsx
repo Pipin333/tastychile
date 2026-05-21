063d77cec9128070b2fcfa52d7f095f5da74a7de
Felipe Riquelme <f.riquelmesalvo@uandresbello.edu>
2026-05-21 15:17:21 -0400

    Ajustados errores de redacción

--- src/app/b2b/page.tsx
+++ src/app/b2b/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       tastingNotes: ["🍫 Chocolate", "🍬 Caramelo", "🍒 Frutas Dulces"]
     },
     {
-      name: "Pronto:Catimore",
+      name: "Pronto: Catimore",
       points: "84 Puntos SCA",
       description: "Un perfil dulce y balanceado con un cuerpo cremoso. Este lote lavado a 1.600 msnm resalta por sus notas ricas que recuerdan a postres clásicos. Una opción sumamente versátil y perfecta tanto para espresso como para filtrados suaves diarios.",
       origin: "Huánuco, Perú",
